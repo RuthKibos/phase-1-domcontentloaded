4d8a289eb5f5653b760b93620d4ee60d15729031
RuthKibos <kibosruth@gmail.com>
2024-04-15 09:20:48 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Code inside this function will run when the DOMContentLoaded event is triggered
+    // Replace the text content of the paragraph with id="text" with "This is really cool!"
+    const paragraph = document.querySelector("#text");
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    }
+    });
